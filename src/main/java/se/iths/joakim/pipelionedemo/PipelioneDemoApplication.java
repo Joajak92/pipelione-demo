7e58304b705b01ed8b9f1656735b0555619a83ae
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-02-12 10:18:00 +0100

    initial commit

---
+++ src/main/java/se/iths/joakim/pipelionedemo/PipelioneDemoApplication.java
@@ -0,0 +1,13 @@
+package se.iths.joakim.pipelionedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelioneDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelioneDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/joakim/pipelionedemo/PipelioneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.joakim.pipelionedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelioneDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
